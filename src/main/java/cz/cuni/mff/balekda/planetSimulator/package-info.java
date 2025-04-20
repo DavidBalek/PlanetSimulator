e75bfe945a371b1c2d4711852bad706c808efd4e
David Balek <davca.balek@gmail.com>
2025-04-20 20:17:59 +0200

    Added documentation of the package and project

---
+++ src/main/java/cz/cuni/mff/balekda/planetSimulator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The Keplerian Simulator functionality
+ * This package includes models for planetary positions, interaction with Horizons API
+ * conversions to RA/Dec and observational parameters like azimuth, altitude or rise/set times.
+ * @since 1.0
+ */
+
+ package cz.cuni.mff.balekda.planetSimulator;
